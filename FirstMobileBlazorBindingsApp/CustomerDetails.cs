71b83202f62165e2e65231b4ab04bc6734f4715a
Emilio <barrera_emilio@yahoo.es>
2020-02-08 19:15:24 +0000

    Add project files.

---
+++ FirstMobileBlazorBindingsApp/CustomerDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstMobileBlazorBindingsApp
+{
+    public class CustomerDetails
+    {
+        public string Name { get; set; }
+
+    }
+}
